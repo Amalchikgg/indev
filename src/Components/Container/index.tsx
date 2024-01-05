c9db0c0e459227d0676e244a07363b3ab2851af2
Amalchikgg <118651451+Amalchikgg@users.noreply.github.com>
2024-01-05 09:47:44 +0500

    Add files via upload

--- src/Components/Container/index.tsx
+++ src/Components/Container/index.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 const Container = ({ children }: Props) => {
   return (
-    <div className='max-w-[1440px] containers w-full m-[0px_auto] px-[70px] tablet:px-[40px] mobile:px-[15px]'>
+    <div className='max-w-[1440px] w-full m-[0px_auto] px-[70px] tablet:px-[40px] mobile:px-[15px]'>
       {children}
     </div>
   );
